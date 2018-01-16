9795ef04673534587002cec855206638a1347765
anusiagunalan <33885473+anusiagunalan@users.noreply.github.com>
2018-01-16 23:26:02 +0800

    Add files via upload

---
+++ Exercise-Function-1.cpp anu.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+}
+
+void masuk_tak()
+{
+	char ans;
+	char c;
+	
+	cout<<endl<< "Dia shoot masuk tak? {answer y-yes atau n-tidak} "<<endl;
+	
+
+	ans='y';
+	while((c!='y')&&(c!='y'))
+	{
+		cin>>ans;
+		if(c!='n' and c!='n')
+		cout<<endl<<"cakap lah bebetul, try again: "<<endl;
+		}
+
+	if(c=='n')
+	{
+		cout<<endl<<"the fudge?!";
+		masuk_tak();
+	}
+	cout<<endl<<"chantek!"<<endl;
+}
+
+
